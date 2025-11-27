600f5d25e2e4ee9330ae0b86aba3f405da58c7f2
AhnChanghyun <ach880203@gmail.com>
2025-11-27 17:55:58 +0900

    React Study

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css'
+import "./App.css";
+import Header from "./component/Header";
+import TodoEditor from "./component/TodoEditor";
+import TodoList from "./component/TodoList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    
-    </>
-  )
+    <div className="App">
+      <Header />
+      <TodoEditor />
+      <TodoList />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ project2/src/component/TodoEditor.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./TodoEditor.css";
+
+const TodoEditor = () => {
+    return (
+        <div className='TodoEditor'>
+            <h4>새로운 Todo 작성하기 👍👍</h4>
+            <div className='editor_wrapper'>
+                <input placeholder='새로운 Todo...'/>
+                <button>추가</button>
+                
+            </div>
+        </div>
+    );
+};
+
+export default TodoEditor;
